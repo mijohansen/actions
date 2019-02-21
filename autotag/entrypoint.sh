32466acb6bb20a45dba34bead98ef6cc8528ad77
mijohansen <michael.johansen@nav.no>
2019-02-21 16:19:21 +0100

    added autotag

---
+++ autotag/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+export PUSH_URL="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+export RELEASE_VERSION="v$(TZ="Europe/Oslo" date +%Y%m%d%H%M)_$(git rev-parse --short HEAD)";
+# If this is not a pull_request, the branch is master and its not tagged. Then create a new tag.
+if [[ "${GITHUB_REF}" = "refs/heads/master" ]]; then
+    git config user.name team-foreldrepenger[bot]
+    git config user.email team-foreldrepenger[bot]@users.noreply.github.com
+    git checkout master
+    git tag ${RELEASE_VERSION}
+    git push --tags ${PUSH_URL} HEAD:master
+    echo "Pushing the new version to $GITHUB_REPOSITORY as $GITHUB_ACTOR"
+else
+    echo "Will not bump version on branch ${GITHUB_REF}"
+fi
